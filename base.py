e6a44397569a5e5875972e7384c54d20d9c99eed
mohamed alaa <mozamezo638@gmail.com>
2022-11-19 01:04:16 +0200

    new file:   file0.txt 	new file:   file1.txt 	new file:   file10.txt 	new file:   file2.txt 	new file:   file3.txt 	new file:   file4.txt 	new file:   file5.txt 	new file:   file6.txt 	new file:   file7.txt 	new file:   file8.txt 	new file:   file9.txt

--- base.py
+++ base.py
@@ -1,1 +1,35 @@
-print("HI")
+
+import os
+import nltk
+from nltk.tokenize import sent_tokenize , word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem.arlstem import ARLSTem 
+from langdetect import detect 
+from nltk.corpus import stopwords
+file = open("file0.txt").read().lower() #Case Folding with lower function
+#################################################
+# /// automatic file creation
+# for i in range(11): 
+#   f = open(f"file{i}.txt","a")
+##################################################
+
+# Tokenization step
+# print(word_tokenize(text))
+token =nltk.Text(word_tokenize(file))
+print(token)
+##################################################
+# removing stopwords
+stp = set(stopwords.words("english"))
+stp.remove("at")
+stp.remove("to")
+filtered_text = [t for t in token if not t in stp]
+# ##################################################
+# # English Stemming step
+stemmized_words = [] 
+stemmer = PorterStemmer()
+for ft in filtered_text:
+  stemmized_words.append(stemmer.stem(ft))
+# ##################################################
+print(stemmized_words)
+
+
