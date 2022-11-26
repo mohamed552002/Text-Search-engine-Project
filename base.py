ae6762767b0fc0c7b680b240fef8b862beb9a0ad
mohamed alaa <mozamezo638@gmail.com>
2022-11-26 13:59:20 +0200

    modified:   base.py 	deleted:    dict.py 	deleted:    file0.txt 	modified:   file1.txt 	modified:   file10.txt 	modified:   file2.txt 	modified:   file3.txt 	modified:   file4.txt 	modified:   file5.txt 	modified:   file6.txt 	modified:   file7.txt 	modified:   file8.txt 	modified:   file9.txt

--- base.py
+++ base.py
@@ -3,7 +3,6 @@ import os
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-from nltk.stem.arlstem import ARLSTem 
 from nltk.corpus import stopwords
 #function to get positional index
 def positional_index(word , lst):
@@ -22,7 +21,7 @@ def text_processing(text):
   #stopword removal
   stp = list(stopwords.words("english"))
   stp+= ",","?","'","\"" # Adding those puctuations to stopword list
-  stp.remove("at")
+  stp.remove("in")
   stp.remove("to")
   filtered_text = [t for t in token if not t in stp and t.isalpha() ] # isalpha here to add only strings
   ####################################################
@@ -34,25 +33,21 @@ def text_processing(text):
   return stemmized_words
   ######################################################
 
-
 #################################################
 # /// automatic file creation
 positional_dict={}
 raw_input=input("what you search for : ")
-counter=0
 final_text=text_processing(raw_input)
 #those loops to get the positional index
 for i in range(0,len(final_text)): 
   positional_dict.update({final_text[i]:[{}]})
-  for j in range(11):
+  for j in range(1,11):
     file = open(f"file{j}.txt").read()
     final_file=text_processing(file)
     if(final_file.count(final_text[i])):
-      positional_dict[f"{final_text[i]}"][0].update({f"file{counter}":[]})
+      positional_dict[f"{final_text[i]}"][0].update({f"file{j}":[]})
       pos_index=(positional_index(final_text[i],final_file))
       for index in pos_index[final_text[i]]:
-        positional_dict[final_text[i]][0][f"file{counter}"].append(index)
-      counter+=1
-      
+        positional_dict[final_text[i]][0][f"file{j}"].append(index)
 # x.append((f"{inputU[j]},file{i}:{pos_index[inputU[j]]}"))
 print(positional_dict) #to print all positional indices
